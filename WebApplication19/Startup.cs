871993cc98eea090ea7c31cb1704619c8f00b71a
RodolfoFadino <rodolfo.fadino@gmail.com>
2019-01-06 18:03:55 -0200

    health check exmaples

--- WebApplication19/Startup.cs
+++ WebApplication19/Startup.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebApplication19.Checks;
 
 namespace WebApplication19
 {
@@ -31,6 +38,10 @@ namespace WebApplication19
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            //services.AddHealthChecks().AddGCInfoCheck("GCInfo");
+
+            services.AddHealthChecks()
+                 .AddCheck("MyDatabase", new SqlConnectionHealthCheck(Configuration["ConnectionStrings:DefaultConnection"]));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -49,6 +60,14 @@ namespace WebApplication19
                 app.UseHsts();
             }
 
+            app.UseHealthChecks("/check");
+
+            //app.UseHealthChecks("/check", new HealthCheckOptions()
+            //{
+            //    // This custom writer formats the detailed status as JSON.
+            //    ResponseWriter = WriteResponse,
+            //});
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
@@ -60,5 +79,22 @@ namespace WebApplication19
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+        private static Task WriteResponse(HttpContext httpContext, HealthReport result)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            var json = new JObject(
+                new JProperty("status", result.Status.ToString()),
+                new JProperty("results", new JObject(result.Entries.Select(pair =>
+                    new JProperty(pair.Key, new JObject(
+                        new JProperty("status", pair.Value.Status.ToString()),
+                        new JProperty("description", pair.Value.Description),
+                        new JProperty("data", new JObject(pair.Value.Data.Select(p => new JProperty(p.Key, p.Value))))))))));
+            return httpContext.Response.WriteAsync(json.ToString(Formatting.Indented));
+        }
     }
+
+
+
 }
